6d0da425aafd76eb2272efdf3add590258f8300f
agus_prayogi <agusyogi600@gmail.com>
2020-05-10 11:07:20 +0700

    View_Pager2

--- app/src/main/java/id/agusprayogi02/pabarcovid19/MainActivity.kt
+++ app/src/main/java/id/agusprayogi02/pabarcovid19/MainActivity.kt
@@ -2,11 +2,24 @@ package id.agusprayogi02.pabarcovid19
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.material.tabs.TabLayoutMediator
+import id.agusprayogi02.pabarcovid19.adapter.ViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val menuText = arrayOf("Home", "Pemeriksaan", "Berita", "Profil")
+    val menuIcon = arrayOf(R.drawable.ic_home, R.drawable.ic_check_up, R.drawable.ic_news, R.drawable.ic_profile)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ViewPagerAdapter(this)
+        view_pager.adapter = adapter
+        TabLayoutMediator(TabLayout,view_pager, TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+            tab.text = menuText[position]
+            tab.icon = ResourcesCompat.getDrawable(resources,menuIcon[position],null)
+        }).attach()
     }
 }
---
+++ app/src/main/java/id/agusprayogi02/pabarcovid19/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,28 @@
+package id.agusprayogi02.pabarcovid19.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import id.agusprayogi02.pabarcovid19.ui.CheckUpFragment
+import id.agusprayogi02.pabarcovid19.ui.HomeFragment
+import id.agusprayogi02.pabarcovid19.ui.NewsFragment
+import id.agusprayogi02.pabarcovid19.ui.ProfilFragment
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+
+    private val JML_Menu = 4
+
+    override fun getItemCount(): Int {
+        return JML_Menu
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when(position){
+            0->{return HomeFragment()}
+            1->{return CheckUpFragment()}
+            2->{return NewsFragment()}
+            3->{return ProfilFragment()}
+            else->{return HomeFragment()}
+        }
+    }
+}
