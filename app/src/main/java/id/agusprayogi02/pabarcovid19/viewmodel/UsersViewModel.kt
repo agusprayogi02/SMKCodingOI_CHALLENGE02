da39a0698c29b370e5f9ee8865b083df9e86b83a
agus_prayogi <agusyogi600@gmail.com>
2020-06-22 14:00:08 +0700

    modifikasi check up

---
+++ app/src/main/java/id/agusprayogi02/pabarcovid19/viewmodel/UsersViewModel.kt
@@ -0,0 +1,23 @@
+package id.agusprayogi02.pabarcovid19.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import id.agusprayogi02.pabarcovid19.database.AppDatabase
+import id.agusprayogi02.pabarcovid19.database.entity.UsersModel
+import id.agusprayogi02.pabarcovid19.repository.UsersRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class UsersViewModel() : ViewModel(){
+    lateinit var repository: UsersRepository
+
+    fun init(context: Context){
+        val usersDao = AppDatabase.getDatabase(context).Users()
+        repository = UsersRepository(usersDao)
+    }
+
+    fun addData(user:UsersModel) = viewModelScope.launch(Dispatchers.IO){
+        repository.insertUsers(user)
+    }
+}
