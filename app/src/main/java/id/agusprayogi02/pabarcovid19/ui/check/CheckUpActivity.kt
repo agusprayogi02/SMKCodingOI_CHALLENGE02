7b971b02e29540b1837a071ecfec9676d3af4923
agus_prayogi <agusyogi600@gmail.com>
2020-06-22 20:15:57 +0700

    add Activity CheckUp

---
+++ app/src/main/java/id/agusprayogi02/pabarcovid19/ui/check/CheckUpActivity.kt
@@ -0,0 +1,12 @@
+package id.agusprayogi02.pabarcovid19.ui.check
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.agusprayogi02.pabarcovid19.R
+
+class CheckUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_up)
+    }
+}
